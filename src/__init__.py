50259072a0a337d7d169458c8674c040f20ce1c4
Andrew Han <han00116@algonquinlive.com>
2023-11-20 11:08:24 -0500

    part 3 fully complete some issues with chmod

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
 # src/__init__.py
 
-import os  # new
 
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new
 
 
 # instantiate the app
@@ -12,17 +13,32 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app_settings = os.getenv('APP_SETTINGS')  # new
-app.config.from_object(app_settings)      # new
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Ping(Resource):
     def get(self):
         return {
             'status': 'success',
-            'message': 'pong! pong pong'
+            'message': 'pong!'
         }
-#import sys
-#print(app.config, file=sys.stderr)
+
 
 api.add_resource(Ping, '/ping')
