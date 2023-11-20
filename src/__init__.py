93747342e18d8c45e3c50b55b0c6134fc7f96d4d
Andrew Han <han00116@algonquinlive.com>
2023-11-20 10:55:46 -0500

    part 2 working fully, print statement commented

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 # src/__init__.py
 
+import os  # new
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
@@ -11,7 +12,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('src.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
 
 
 class Ping(Resource):
@@ -20,6 +22,7 @@ class Ping(Resource):
             'status': 'success',
             'message': 'pong! pong pong'
         }
-
+#import sys
+#print(app.config, file=sys.stderr)
 
 api.add_resource(Ping, '/ping')
